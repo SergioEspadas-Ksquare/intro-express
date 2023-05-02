a6063d5f0065aca9f1c64d8a53b7526d9471e1c0
Carlos Reyes <carlos.reyes@theksquaregroup.com>
2023-05-02 12:33:39 -0500

    exercise setup

---
+++ index.js
@@ -0,0 +1,79 @@
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+const HOST_NAME = "localhost";
+
+const v1 = express.Router();
+
+const userRouter = express.Router();
+
+// LIST
+userRouter.get("/", (req, res) => {
+  res.send("GET /v1/users");
+});
+
+// CREATE
+userRouter.post("/", (req, res) => {
+  res.send("POST v1/users");
+});
+
+// READ
+userRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.send(`GET /v1/users/${id}`);
+});
+
+// UPDATE
+userRouter.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.send(`PUT /v1/users/${id}`);
+});
+
+// DELETE
+userRouter.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.send(`DELETE /v1/users/${id}`);
+});
+
+v1.use("/users", userRouter);
+app.use("/v1", v1);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, HOST_NAME, () => {
+  console.log(`Server is listening at port: ${PORT}`);
+});
+
+// app.method(PATH, HANDLER) <--- Secret sauce
+
+/* Interface User
+    Id
+    Name
+    Age
+    OrgId
+*/
+
+/* Interface Organization
+    Id
+    Name
+*/
+
+// (L)CRUD Users
+// [GET] /users
+// [POST] /users
+// [GET] /users/{userId}
+// [PUT] /users/{userId}
+// [DELETE] /users/{userId}
+
+// (L)CRUD Orgs
+
+// [GET] /orgs
+// [POST] /orgs
+// [GET] /orgs/{orgId}
+// [PUT] /orgs/{orgId}
+// [DELETE] /orgs/{orgId}
